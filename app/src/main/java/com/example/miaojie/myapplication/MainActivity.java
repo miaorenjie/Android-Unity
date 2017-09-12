76bda47dfe843049b7d2b6c9fb3eedaf3e13ed02
miaorenjie <501216475@qq.com>
2017-09-12 23:15:20 +0800

    unity加入android

---
+++ app/src/main/java/com/example/miaojie/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.miaojie.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+    protected UnityPlayer unityPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unityPlayer=new UnityPlayer(this);
+        View view=mUnityPlayer.getView();
+        LinearLayout linearLayout=findViewById(R.id.content);
+        linearLayout.addView(view);
+    }
+}
